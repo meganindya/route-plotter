092f34a32e45bed5814e24d30e34cb39831b63b2
Anindya Kundu <anindya.k22@outlook.com>
2021-02-14 15:52:03 +0530

    docs: add comments

--- run.py
+++ run.py
@@ -4,8 +4,17 @@ import find_shortest_path
 import guide_bot
 import time
 
+# solidify the non-path region by converting to contrasting color
 solidify.main()
+
+# separate the path from the solidified image, find the skeleton, convert to graph
 gen_graph.main()
+
+# find shortest path between the starting and ending point of the graph
 find_shortest_path.main()
+
+# plot the path on the image
 guide_bot.main()
-if 0xFF == ord('q'):    quit
+
+if 0xFF == ord('q'):
+    quit
